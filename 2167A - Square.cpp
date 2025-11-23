f8f6477532f6bde71c51fb28e759f02866b6e9ff
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-11-23 07:54:33 +0530

    Add files via upload

---
+++ 2167A - Square.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    int a,b,c,d;
+    cin >> t;
+    for (int i =0 ; i < t ; i++){
+        cin >>a >> b >> c >> d;
+        if(a == b && a == c && a == d){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    
+    
+    return 0;
+}
