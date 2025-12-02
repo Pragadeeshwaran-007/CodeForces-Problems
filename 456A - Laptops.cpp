d4587be738ee7f2e43a848dd4d5c263bcef980fb
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-12-02 22:16:46 +0530

    Add files via upload

---
+++ 456A - Laptops.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int,int>> laptops(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> laptops[i].first >> laptops[i].second;  // price, quality
+    }
+
+    sort(laptops.begin(), laptops.end());
+
+    for(int i = 1; i < n; i++) {
+        if(laptops[i].second < laptops[i - 1].second) {
+            cout << "Happy Alex";
+            return 0;
+        }
+    }
+
+    cout << "Poor Alex";
+    return 0;
+}
