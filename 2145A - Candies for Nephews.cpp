d87bf2d79609421743f377591be0446eab565349
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-12-11 22:55:41 +0530

    Add files via upload

---
+++ 2145A - Candies for Nephews.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+     int n;
+     cout << n%3;
+        
+    }
+    return 0;
+}
