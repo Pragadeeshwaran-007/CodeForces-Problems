6d231c3e510a58bc23b91e166ca90ab9aed34c45
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-12-23 21:26:50 +0530

    Add files via upload

---
+++ 118A - String Task.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;	
+	vector<char> arr;
+	for(int i = 0 ; i < s.length(); i++){
+		char ch = tolower(s[i]);
+		if(ch == 'a' || ch == 'e' ||  ch == 'i' || ch =='o' || ch == 'u') continue;
+		
+		else{
+			arr.push_back(ch);
+		}
+	}
+	for(int i = 0 ; i < arr.size(); i++){
+		cout << "." << arr[i];
+	} 
+	return 0;
+}
