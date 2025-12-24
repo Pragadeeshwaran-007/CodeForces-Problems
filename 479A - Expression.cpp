93d072d40460a944e3300c8115ce1d88e8eea0ab
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-12-24 21:34:16 +0530

    Add files via upload

--- 479A - Expression.cpp
+++ 479A - Expression.cpp
@@ -1,22 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    vector<int> arr(3);
-    for(int i=0;i<3;i++) {
-        cin >> arr[i];
-    }
-    int sum = arr[0];
-    sort(arr.begin(), arr.end());
-    for (int i = 1; i < 3; i++) {
-        if (sum + arr[i]  > sum * arr[i]) {
-            sum = sum + arr[i];
-        }
-        else {
-            sum = sum * arr[i];
-        }
-    }
+    int a, b, c;
+    cin >> a >> b >> c;
 
-    cout << sum << endl;
+    int ans = max({
+        a + b + c,
+        a * b * c,
+        (a + b) * c,
+        a * (b + c),
+        a + b * c,
+        a * b + c
+    });
 
+    cout << ans << endl;
     return 0;
 }
