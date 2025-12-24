c25653f56335771a12c630fa40a9397ab9a60eab
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2025-12-24 22:34:07 +0530

    Add files via upload

---
+++ 580A - Kefa and First Steps.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+    int count = 1, m = 1;
+    for(int i=1;i<n;i++) {
+        if (arr[i] > arr[i-1]) {
+            count++;
+        }
+        else {
+            m = max(m,count);
+            count = 1;
+        }
+    }
+    cout << m << endl;
+
+    return 0;
+}
