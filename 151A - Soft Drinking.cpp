b3dba9f81a3bad103249377f47533bf9bb2c9d46
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-01-12 22:01:07 +0530

    Add files via upload

---
+++ 151A - Soft Drinking.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    int total_drink = k * l;
+    int total_lime_slices = c * d;
+    int toasts_by_drink = total_drink / nl;
+    int toasts_by_lime = total_lime_slices;
+    int toasts_by_salt = p / np;
+    int total_toasts = min({toasts_by_drink, toasts_by_lime, toasts_by_salt});
+    cout << total_toasts / n << endl;
+    return 0;
+}
