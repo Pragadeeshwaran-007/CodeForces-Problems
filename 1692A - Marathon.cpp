1f6f4256c9a4c581eebce4aac0135aff87da10ec
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-01-22 22:53:16 +0530

    Add files via upload

---
+++ 1692A - Marathon.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int cnt = 0;
+        if(b > a) cnt++;
+        if(c > a) cnt++;
+        if(d > a) cnt++;
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
