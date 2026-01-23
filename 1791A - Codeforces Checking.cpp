4e25f1d361e2431ea86b9635bff630bfe2fd0231
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-01-23 21:52:46 +0530

    Add files via upload

---
+++ 1791A - Codeforces Checking.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    string s = "codeforces";
+    
+    while (t--) {
+        char c;
+        cin >> c;
+        
+        bool found = false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == c) {
+                found = true;
+                break;
+            }
+        }
+        
+        if (found) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
