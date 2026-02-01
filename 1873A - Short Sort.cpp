1c0b860c5d17106fce653d57a3304878325e01c8
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-02-01 19:24:28 +0530

    Add files via upload

---
+++ 1873A - Short Sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        if (s == "bca" || s == "cab") {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+        }
+    }
+    return 0;
+}
