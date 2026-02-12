70f21ba8d03fd689d75a2814e269a0b68538e840
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-02-12 21:41:06 +0530

    Add files via upload

---
+++ 1829A - Love Story.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    string target = "codeforces";
+
+    while (t--) {
+        string s;
+        cin >> s;
+        int diff_count = 0;
+
+        for (int i = 0; i < 10; ++i) {
+            if (s[i] != target[i]) {
+                diff_count++;
+            }
+        }
+
+        cout << diff_count << endl;
+    }
+
+    return 0;
+}
