b464d55115d215081d015c6684342e851cbb3367
Pragadeeshwaran S <pragadeeshwaran098@gmail.com>
2026-09-12 21:13:09 +0530

    Implement Honest Coach solution in C++

---
+++ 1360B - Honest Coach.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> s(n);
+        for (int i = 0; i < n; i++) cin >> s[i];
+        
+        sort(s.begin(), s.end());
+        
+        int ans = INT_MAX;
+        for (int i = 0; i + 1 < n; i++) {
+            ans = min(ans, s[i+1] - s[i]);
+        }
+        
+        cout << ans << "\n";
+    }
+    return 0;
+}
